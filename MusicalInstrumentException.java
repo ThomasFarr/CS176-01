86e363de4ff60f9d188f3d429eb54566e70b8aed
ThomasFarr <114097013+ThomasFarr@users.noreply.github.com>
2023-12-13 14:05:09 -0500

    ProblemSet6

---
+++ MusicalInstrumentException.java
@@ -0,0 +1,14 @@
+package edu.monmouth.MusicalInstrument;
+
+public class MusicalInstrumentException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public MusicalInstrumentException() {
+		super();
+	}
+	
+	public MusicalInstrumentException(String message) {
+		super(message);
+	}
+}
